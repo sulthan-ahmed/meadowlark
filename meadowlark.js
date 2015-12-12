3f13a5c8bbb7863064a438e1585ac6734818ade8
sulthan-ahmed <sulthanahmeduk@gmail.com>
2015-12-12 12:46:28 +0000

    1. Set our template engine as handlebars 2. Set the default layout to use the main file 3. Added routing for / and /about 4. Added handlebars dependency 5. Created a main.handlebars file in the layout folder

--- meadowlark.js
+++ meadowlark.js
@@ -1,12 +1,33 @@
 var express = require('express');
 var app = express();
 
+//in Express, the order in which routes and middleware are added is significant
+
+//set up handlebars as our template engine (i.e. view engine)
+//specified default layout as main. This is the layout for any viewß
+var handlebars = require('express3-handlebars').create({defaultLayout: 'main'})
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 //In many environments (e.g. Heroku), and as a convention, you can set the environment
 // variable PORT to tell your web server what port to listen on.
 //process.env.port || 3000 means: whatever is in the environment variable PORT,
 // or 3000 if there's nothing there.
 app.set('port', process.env.PORT || 3000);
 
+//app. get is a method that takes two parameters: a path and a function.
+app.get('/', function(req, res){
+    res.type('type/plain');
+    res.send('Meadowlark Travel');
+});
+
+app.get('/about', function(req, res){
+    res.type('text/plain');
+    res.send('About Meadowlark Travel');
+});
+
+// Express adds middleware when you use app.use
+// i.e. catch-all handler for anything that didn’t get matched by a route
 // custom 404 page
 app.use(function(req, res){
     res.type('text/plain');
